5454f1ffb052fe82f548cec5ed6021fe88a56a7e
pablo <pablo.cardenas@uca.es>
2025-08-14 19:41:39 +0200

    (PCF) first trial to template

---
+++ ArtistPortfolio/src/Data.js
@@ -0,0 +1,14 @@
+
+const Data = {
+  WebsiteTitle: "JuliaQ.designs",
+  HeaderTitle: "Julia Quintana's Portfolio",
+  FooterText: "All rights reserved",
+  AboutEmail: "juliaquitana@quintana.com",
+  AboutEmailSubject: "email subjetc",
+  AboutHeading: "About me!",
+  AboutTextParagraph1: "Julia Quintana is a Fine Arts student from Chiclana de la Frontera, deeply rooted in her Andalusian heritage and profoundly inspired by the sea. Her work is guided by a passion for digital illustration and concept art, often delving into themes that reflect her own intimate experiences and personal narratives. Through her art, she seeks to merge the visual language of contemporary media with the emotional depth of her coastal upbringing.",
+  AboutTextParagraph2: "Beyond her digital practice, Julia embraces a variety of artistic disciplines, including sculpture, painting, and photography. Among these, analog photography holds a special place in her creative process, allowing her to capture moments with a tactile, timeless quality that resonates with her sensitivity for memory and place.",
+  AboutButtonText: "More about me",
+};
+
+export default Data;
